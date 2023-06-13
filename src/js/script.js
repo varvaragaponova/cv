5f4d5df6683eb900630d972ae3c116200a300c63
Varvara Gaponova <gaponova.varvara21091995@gmail.com>
2023-06-13 23:11:38 +0400

    fix: change menu functionality

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,22 @@
 const menu = document.querySelector(".menu");
 const menuBurger = document.querySelector(".menu-burger");
 const overlay = document.querySelector(".overlay");
+const body = document.querySelector("body");
 
 menuBurger.addEventListener('click', function() {
     menu.classList.toggle('menu_open');
     menuBurger.classList.toggle('menu-burger_open');
     overlay.classList.toggle('overlay_active');
+    body.classList.toggle('no_scroll');
+    event.stopPropagation();
 })
+
+document.body.addEventListener('click', function(event) {
+    if (event.target === menu) return;
+    if (menu.classList.contains('menu_open')) {
+        menu.classList.remove('menu_open');
+        menuBurger.classList.remove('menu-burger_open');
+        body.classList.remove('no_scroll');
+        overlay.classList.remove('overlay_active');
+    }
+});
